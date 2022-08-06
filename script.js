8f17c75a1d15ce04ae98dcbececacd96264fd8f5
nasir ali <a.nasirali@outlook.com>
2022-08-06 15:08:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
 /* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */
+
+
+var roll, dice, sum, item;
+
+// Describe this function...
+function Play_Dice() {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_list = document.getElementById('list');
+  element_list.replaceChildren();
+  roll.forEach((item) => {
+    let new_li = document.createElement('li');
+    new_li.innerText = item;
+
+    element_list.appendChild(new_li);
+  });
+  sum = roll.reduce((a,b) => a+b, 0);
+  let element_total = document.getElementById('total');
+  element_total.innerText = sum;
+  if (sum == 11) {
+    let element_info = document.getElementById('info');
+    element_info.innerText = 'Winner chicken DINNER';
+  } else if (sum > 11) {
+    let element_info2 = document.getElementById('info');
+    element_info2.innerText = 'lost, NO chicken DINNER';
+  } else {
+    let element_info3 = document.getElementById('info');
+    element_info3.innerText = 'Still in game';
+  }
+}
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+roll = [];
+dice = [1, 2, 3, 4, 5, 6];
+
+
+document.getElementById('button_roll').addEventListener('click', (event) => {
+  roll.push(randomMember(dice));
+  Play_Dice();
+
+});
+
+document.getElementById('button_restart').addEventListener('click', (event) => {
+  roll = [];
+  let element_list2 = document.getElementById('list');
+  element_list2.replaceChildren();
+  let element_total2 = document.getElementById('total');
+  element_total2.replaceChildren();
+  element_total2.innerText = 0;
+  let element_info4 = document.getElementById('info');
+  element_info4.innerText = 'Still in game';
+
+});
+
+document.getElementById('button_remove').addEventListener('click', (event) => {
+  roll.pop();
+  Play_Dice();
+
+});
+
+
